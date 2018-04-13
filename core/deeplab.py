1ab1d0ea232b6efb90f16c9e76136e312b744ff5
Nikolai Timonin <nikki.timonin@gmail.com>
2018-04-13 14:29:32 +0300

    Add xception function

--- core/deeplab.py
+++ core/deeplab.py
@@ -2,22 +2,87 @@ import tensorflow as tf
 from tensorflow.contrib import slim
 
 
-# def xception():
-#
-# def flow():
-#
-# def xception_block():
-#
-# def unit():
-#
+def xception(inputs,
+             scope):
+    with tf.variable_scope(scope, 'xception', [inputs]):
+        with slim.arg_scope([slim.separable_conv2d,
+                             slim.conv2d],
+                            biases_initializer=None):
+            inputs = xception_block(inputs,
+                                    depth_list=[128, 128, 128],
+                                    skip_connetcion_type='conv',
+                                    stride=2,
+                                    separable_conv_activation_fn=False,
+                                    num_units=1,
+                                    scope='entry_flow/block1')
 
+            inputs = xception_block(inputs,
+                                    depth_list=[256, 256, 256],
+                                    skip_connetcion_type='conv',
+                                    stride=2,
+                                    separable_conv_activation_fn=False,
+                                    num_units=1,
+                                    scope='entry_flow/block2')
+
+            inputs = xception_block(inputs,
+                                    depth_list=[728, 728, 728],
+                                    skip_connetcion_type='conv',
+                                    stride=1,
+                                    separable_conv_activation_fn=False,
+                                    num_units=1,
+                                    scope='entry_flow/block3')
+
+            inputs = xception_block(inputs,
+                                    depth_list=[728, 728, 728],
+                                    skip_connetcion_type='add',
+                                    stride=1,
+                                    separable_conv_activation_fn=False,
+                                    num_units=16,
+                                    scope='middle_flow/block1')
+
+            inputs = xception_block(inputs,
+                                    depth_list=[728, 1024, 1024],
+                                    skip_connetcion_type='conv',
+                                    stride=1,
+                                    separable_conv_activation_fn=False,
+                                    num_units=1,
+                                    scope='exit_flow/block1')
+
+            inputs = xception_block(inputs,
+                                    depth_list=[1536, 1536, 2048],
+                                    skip_connetcion_type='none',
+                                    stride=1,
+                                    separable_conv_activation_fn=False,
+                                    num_units=1,
+                                    scope='exit_flow/block2')
+
+    return inputs
+
+
+
+def xception_block(inputs,
+                   depth_list,
+                   skip_connetcion_type,
+                   stride,
+                   separable_conv_activation_fn,
+                   num_units,
+                   scope):
+    with tf.variable_scope(scope, 'block', [inputs]):
+        for i in range(num_units):
+            inputs = xception_module(inputs,
+                                      depth_list,
+                                      skip_connetcion_type,
+                                      stride=stride,
+                                      separable_conv_activation_fn=separable_conv_activation_fn,
+                                      scope='unit_'+str(i+1) + '/xception_module')
+    return inputs
 
 def xception_module(inputs,
                     depth_list,
                     skip_connection_type,
                     stride,
                     rate=1,
-                    separable_conv_activaation_fn=False,
+                    separable_conv_activation_fn=False,
                     scope=None):
     if len(depth_list) != 3:
         raise ValueError('Expect three elements in depth_list')
@@ -29,7 +94,7 @@ def xception_module(inputs,
                           stride,
                           rate,
                           scope):
-        if separable_conv_activaation_fn:
+        if separable_conv_activation_fn:
             activation_fn = tf.nn.relu
         else:
             inputs = tf.nn.relu(inputs)
@@ -51,24 +116,24 @@ def xception_module(inputs,
                                          num_outputs=depth_list[i],
                                          kernel_size=3,
                                          depth_multiplier=1,
-                                         stride=stride,
+                                         stride=stride if i == 2 else 1,
                                          rate=rate,
                                          scope='separable_conv' + str(i+1))
 
-    if skip_connection_type == 'conv':
-        inputs = slim.conv2d(inputs,
-                            num_outputs=depth_list[-1],
-                            kernel_size=[1, 1],
-                            stride=stride,
-                            activation_fn=None,
-                            scope='shortcut')
-        return residual + inputs
-    elif skip_connection_type == 'add':
-        return residual + inputs
-    elif skip_connection_type == 'none':
-        return residual
-    else:
-        raise ValueError('Unsupported skip connection type.')
+        if skip_connection_type == 'conv':
+            inputs = slim.conv2d(inputs,
+                                num_outputs=depth_list[-1],
+                                kernel_size=[1, 1],
+                                stride=stride,
+                                activation_fn=None,
+                                scope='shortcut')
+            return residual + inputs
+        elif skip_connection_type == 'add':
+            return residual + inputs
+        elif skip_connection_type == 'none':
+            return residual
+        else:
+            raise ValueError('Unsupported skip connection type.')
 
 
 
--- core/preprocess_utills.py
+++ core/preprocess_utills.py
@@ -2,16 +2,16 @@ import tensorflow as tf
 from tensorflow.contrib import slim
 from core import deeplab
 
-with tf.Graph().as_default():
+ckpt_dir = '/Users/nikki/Desktop/models/research/deeplab/graph/1/'
+
+with tf.Graph().as_default() as g:
     inputs = tf.placeholder(tf.float32, shape=(None, 257, 257, 64))
-    outputs = deeplab.xception_module(inputs,
-                                      depth_list=[128, 128, 128],
-                                      skip_connection_type='conv',
-                                      stride=1,
-                                      rate=1,
-                                      separable_conv_activaation_fn=False,
-                                      scope=None)
+
+    outputs = deeplab.xception(inputs,
+                               scope='xception_65')
 
     for v in slim.get_model_variables():
         print('name={}, shape={}'.format(v.name, v.get_shape()))
+
+    writer = tf.summary.FileWriter(logdir=ckpt_dir+'3', graph=g)
 
