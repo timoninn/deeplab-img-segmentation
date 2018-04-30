7516065e089eb4cbf2f8dc090ecd5e8c6580d67d
Nikolai Timonin <nikki.timonin@gmail.com>
2018-04-30 21:37:00 +0300

    Add Logits class to core.model

--- core/model.py
+++ core/model.py
@@ -1,16 +1,20 @@
 import tensorflow as tf
+from tensorflow.contrib import slim
 
 FROSEN_INFERENCE_GRAPH_DIR_PATH = '../frozen_inference_graph/deeplabv3_pascal_train_aug/'
+
 
 class DeepLab(object):
     INPUT_TENSOR_NAME = 'add_2:0'
     SEMATIC_PREDICITON_TENSOR_NAME = 'logits/semantic/BiasAdd:0'
     DECODER_OUTPUT_TENSOR_NAME = 'decoder/decoder_conv1_pointwise/Relu:0'
 
-    def __init__(self):
+    def __init__(self, path=None):
         self.graph = tf.Graph()
 
-        file = open(FROSEN_INFERENCE_GRAPH_DIR_PATH + 'frozen_inference_graph.pb', mode='rb')
+        path = FROSEN_INFERENCE_GRAPH_DIR_PATH + 'frozen_inference_graph.pb' if path==None else path
+
+        file = open(path, mode='rb')
         graph_def = tf.GraphDef.FromString(file.read())
         file.close()
 
@@ -29,3 +33,8 @@ class DeepLab(object):
     def run_semantic(self, inputs):
         return self.sess.run(self.SEMATIC_PREDICITON_TENSOR_NAME,
                              feed_dict={self.INPUT_TENSOR_NAME: inputs})
+
+
+class Logits(object):
+    def run(self, inputs):
+        return slim.conv2d(inputs, num_outputs=8, kernel_size=1, activation_fn=None)
