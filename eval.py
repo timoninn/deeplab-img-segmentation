5854e0b26cf6123a90ee7b9b07f9ce4a7bb12891
Nikolai Timonin <nikki.timonin@gmail.com>
2018-05-01 17:51:51 +0300

    Setup miou metric evaluation

--- eval.py
+++ eval.py
@@ -1,53 +1,59 @@
 import tensorflow as tf
 from tensorflow.contrib import slim
 
-import matplotlib.pyplot as plt
+import dataset.utils as dutils
+from core import model
 
-
-def model(inputs):
-    return slim.conv2d(inputs, num_outputs=8, kernel_size=1, activation_fn=None)
-
-
-def _map_function(x):
-    feature = {'train/labels': tf.FixedLenFeature([], tf.string),
-               'train/prelogits': tf.FixedLenFeature([], tf.string)}
-
-    features = tf.parse_single_example(x, features=feature)
-
-    label = tf.decode_raw(features['train/labels'], tf.uint8)
-    prelogit = tf.decode_raw(features['train/prelogits'], tf.float32)
-
-    label = tf.reshape(label, shape=[129, 129])
-    prelogit = tf.reshape(prelogit, shape=[129, 129, 256])
-
-    return (label, prelogit)
-
-
-dataset = tf.data.TFRecordDataset(['dataset/prelogits.tfrecords'])
-dataset = dataset.map(map_func=_map_function)
+dataset = tf.data.TFRecordDataset(['tmp/prelogits.tfrecord'])
+dataset = dataset.map(map_func=dutils.parse_tfexample_to_decoder_seg)
 dataset = dataset.repeat(1)
-dataset = dataset.shuffle(buffer_size=3)
-dataset = dataset.batch(1)
+dataset = dataset.batch(9)
+
+logits = model.Logits()
 
 with tf.Graph().as_default():
     iterator = dataset.make_one_shot_iterator()
-    label, prelogit_ = iterator.get_next()
-    predictions = model(inputs=prelogit_)
-    prediction = tf.argmax(predictions, axis=3)
+    dec_output, seg_image = iterator.get_next()
+    predictions = logits.run(dec_output)
+    predictions = tf.argmax(predictions, axis=3)
 
-    model_path = tf.train.latest_checkpoint('tmp/model/')
-    init_fn = slim.assign_from_checkpoint_fn(model_path,
-                                             slim.get_model_variables(),
-                                             ignore_missing_vars=True)
+    seg_image = tf.reshape(seg_image, shape=[-1])
+    predictions = tf.reshape(predictions, shape=[-1])
 
-    with tf.Session() as sess:
-        init_fn(sess)
+    # Define evaluation metric.
+    metric_map = {}
+    metric_map['miou'] = tf.metrics.mean_iou(seg_image,
+                                             predictions=predictions,
+                                             num_classes=8)
 
-        while True:
-            label1, result = sess.run([label, prediction])
+    metrics_to_values, metrics_to_updates = tf.contrib.metrics.aggregate_metric_map(metric_map)
 
-            plt.imshow(label1[0])
-            plt.show()
+    for metric_name, metric_value in metrics_to_values.items():
+        slim.summaries.add_scalar_summary(metric_value,
+                                          name=metric_name,
+                                          print_summary=True)
 
-            plt.imshow(result[0])
-            plt.show()
+    tf.logging.set_verbosity(tf.logging.INFO)
+    slim.evaluation.evaluation_loop(master='',
+                                    checkpoint_dir='tmp/model/',
+                                    logdir='tmp/eval_log_dir/',
+                                    num_evals=5,
+                                    eval_op=list(metrics_to_updates.values()),
+                                    eval_interval_secs=1)
+
+    # model_path = tf.train.latest_checkpoint('tmp/model/')
+    # init_fn = slim.assign_from_checkpoint_fn(model_path,
+    #                                          slim.get_model_variables(),
+    #                                          ignore_missing_vars=True)
+    #
+    # with tf.Session() as sess:
+    #     sess.run(tf.global_variables_initializer())
+    #     sess.run(tf.local_variables_initializer())
+    #
+    #     init_fn(sess)
+    #
+    #     while True:
+    #         sess.run(list(metrics_to_updates.values()))
+    #
+    #         metric_values = sess.run(list(metrics_to_values.values()))
+    #         print(metric_values)
